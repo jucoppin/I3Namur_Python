7ea20d131cd786baca92112718b9f8ef16846981
PaulineDo <dosimontp@gmail.com>
2021-07-16 15:42:20 +0200

    améliorations incroyables

--- Nous/Exercice.py
+++ Nous/Exercice.py
@@ -1,8 +1,10 @@
 from turtle import turtles
 
+
 def phone_number(phone_numbers):
     return len({num[:i] for num in phone_numbers for i in range(1, len(num)+1)})
-    return len({num[:i] for num in phone_numbers for i in range(1, len(num)+1)})
+   # return len({num[:i] for num in phone_numbers for i in range(1, len(num)+1)})
+
 
 a, b = 0
 c = a / b
@@ -10,3 +12,4 @@ c = a / b
 print(c)
 
 print('hello')
+print(phone_number("yoloh"))
