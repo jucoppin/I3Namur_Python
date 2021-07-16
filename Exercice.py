57baab6a8a894e2050bae3f8c7eb2f6deb6d2f5c
warell <elliot.warnon@gmail.com>
2021-07-16 16:12:31 +0200

    Merge

--- Exercice.py
+++ Exercice.py
@@ -1,21 +1,12 @@
-from turtle import turtles
-
-# Essai - Vous pouvez lire ce que j'écris ? iufhurheuighurhiurghiurhgruyighiuy
+# Essai - Vous pouvez lire ce que j'écris ?
 
 
-print(f"5 x 551566498651561544 ={resultat}")
-def phone_number(phone_numbers):
-    return len({num[:i] for num in phone_numbers for i in range(1, len(num)+1)})
-    return len({num[:i] for num in phone_numbers for i in range(1, len(num)+1)})
-   # return len({num[:i] for num in phone_numbers for i in range(1, len(num)+1)})
+nbre_1 = 5
+
+nbre_2 = 55
+
+total = 5*55
+resultat = total
 
 
-a, b = 0
-c = a / b
-
-print(c)
-
-print('hello')
-print(phone_number("yoloh"))
-def phone_number(phone_numbers):
-    return len({num[:i] for num in phone_numbers for i in range(1, len(num)+1)})
+print(f"5 x 55 ={resultat}")
