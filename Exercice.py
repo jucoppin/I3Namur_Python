bfd575bc8cb093cbe4595bb8af4c97bd4d739bef
JuCoppin <julien.coppin@bstorm.be>
2021-07-16 15:59:20 +0200

    try error

--- Exercice.py
+++ Exercice.py
@@ -1,6 +1,9 @@
 from turtle import turtles
 
+# Essai - Vous pouvez lire ce que j'écris ? iufhurheuighurhiurghiurhgruyighiuy
 
+
+print(f"5 x 551566498651561544 ={resultat}")
 def phone_number(phone_numbers):
     return len({num[:i] for num in phone_numbers for i in range(1, len(num)+1)})
     return len({num[:i] for num in phone_numbers for i in range(1, len(num)+1)})
